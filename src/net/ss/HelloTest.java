0f1a31f6e7ddd56b306192025648a68c16ccce33
sooalove <paran0501@gmail.com>
2012-11-12 21:07:58 +0900

    add getMessaget()

--- src/net/ss/HelloTest.java
+++ src/net/ss/HelloTest.java
@@ -3,5 +3,9 @@ package net.ss;
 import junit.framework.TestCase;
 
 public class HelloTest extends TestCase {
-
+	public void testGetMessage() throws Exception {
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Hello World", hello.getMessage());
+		
+	}
 }
---
+++ src/net/ss/HelloWorld.java
@@ -0,0 +1,9 @@
+package net.ss;
+
+public class HelloWorld {
+
+	public String getMessage() {
+		return "Hello World";
+	}
+
+}
